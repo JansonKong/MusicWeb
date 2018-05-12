deeb87d5e2eebf4a364b20dd992a140169c553dc
JansonKong <995710545@qq.com>
2018-05-12 17:15:27 +0800

    更改了首页等

--- src/components/musicDis.vue
+++ src/components/musicDis.vue
@@ -1,9 +1,23 @@
 <template>
   <div>
+    <!-- <ul class="mod_top_subnav">
+      <li class="top_subnav__item" >
+       <router-link to="/" class="top_subnav__link" :class="{'top_subnav__link--current': tagLink==1}">首页</router-link>
+      </li>
+      <li class="top_subnav__item" >
+        <router-link  to="/singer" class="top_subnav__link" :class="{'top_subnav__link--current': tagLink==2}">歌手</router-link>
+      </li>
+      <li class="top_subnav__item" >
+        <router-link  to="/cd" class="top_subnav__link" :class="{'top_subnav__link--current': tagLink==3}">专辑</router-link>
+      </li>
+    <li class="top_subnav__item">
+      <router-link  to="" class="top_subnav__link" :class="{'top_subnav__link--current': tagLink==4}">排行版</router-link>
+    </li>
+  </ul>  -->
     <v-cd></v-cd>
-    <v-event></v-event>
+    <!-- <v-event></v-event>
     <v-toplist></v-toplist>
-    <v-taolist></v-taolist>
+    <v-taolist></v-taolist> -->
   </div>
 </template>
 
@@ -32,5 +46,19 @@
 </script>
 
 <style lang="scss">
-
+//   .mod_top_subnav {
+//   height: 51px;
+//   line-height: 51px;
+//   border-top: 1px solid #f2f2f2;
+//   padding-left: 256px;
+// }
+// .top_subnav__item {
+//   float: left;
+//   font-size: 15px;
+//   color: #c1c1c1;
+//   margin-right: 56px;
+// }
+// .top_subnav__link {
+//   cursor: auto;
+// }
 </style>
